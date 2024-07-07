40accc4c62c758dcd9dc644cbb45ef55ca94558a
vishnusingh3756@gmail.com <vishnusingh3756@gmail.com>
2024-07-07 09:24:45 -0700

    first commit

--- 3rd .python calculater.py
+++ 3rd .python calculater.py
@@ -2,7 +2,8 @@ num1=int(input("enter the number 1:"))
 num2=int(input("enter the number 2:"))
 num3=int(input("enter the number 3:"))
 operator=input ("enter the operator:")
-if operator=="+":                
+if operator=="+":              
+        
             print(num1+num2+num3)
 elif operator=="-":
              print(num1-num2-num3)
